8cf1451534a1a664cc7828a42ec134a1cd9403c5
simondvdv <simondv4@gmail.com>
2024-06-24 14:53:22 +0300

    save commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from module1 import *
 
-test_string1 = '2 + 3 * 4'
+test_string1 = '2 + 13 * 415'
 test_string2 = '(1 + 2) * (3 + 4)'
 test_string3 = '2 * (3 + 4 * (5 - 2))'
 
-math_string_calculator(test_string1)
+print(math_string_calculator(test_string1))
--- module1.py
+++ module1.py
@@ -2,8 +2,34 @@ def string_splitter(math_string):
     pass
 
 
+def number_checker(math_string, index):
+    left_index = 0
+    right_index = 0
+    for i in range(index):
+        if not (0 <= ord(math_string[index - i]) <= 9):
+            left_index = i
+    for i in range(index):
+        if not (0 <= ord(math_string[index + i]) <= 9):
+            right_index = i
+    return left_index, right_index
+
+
 def math_string_calculator(math_string):
     math_string = math_string.replace(' ', '')
+    if not ('*' in math_string or '/' in math_string or '+' in math_string or '-' in math_string):
+        print('работает')
+        return 1
     if '*' in math_string:
-        print(math_string.find('*'))
+        index = math_string.find('*')
+        l_index, r_index = number_checker(math_string, index)
+        return 1
+    if '/' in math_string:
+        index = math_string.find('/')
+        pass
+    if '+' in math_string:
+        index = math_string.find('+')
+        pass
+    if '-' in math_string:
+        index = math_string.find('-')
+        pass
     print(math_string)
