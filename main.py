b35a6035188bbe09dbd12dfd03980c71d50fbaa8
simondv4 <simondv4@gmail.com>
2024-06-24 13:12:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from module1 import *
+
+test_string1 = '2 + 3 * 4'
+test_string2 = '(1 + 2) * (3 + 4)'
+test_string3 = '2 * (3 + 4 * (5 - 2))'
+
+math_string_calculator(test_string1)
---
+++ module1.py
@@ -0,0 +1,6 @@
+def string_splitter(math_string):
+    pass
+
+def math_string_calculator(math_string):
+    math_string = math_string.replace(' ', '')
+    print(math_string)
