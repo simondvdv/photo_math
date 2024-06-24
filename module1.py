9da7a1d07fd8219a8f2fd1906afe3e660c5915af
simondv4 <simondv4@gmail.com>
2024-06-24 13:27:03 +0300

    to notebook

--- module1.py
+++ module1.py
@@ -1,6 +1,9 @@
 def string_splitter(math_string):
     pass
 
+
 def math_string_calculator(math_string):
     math_string = math_string.replace(' ', '')
+    if '*' in math_string:
+        print(math_string.find('*'))
     print(math_string)
